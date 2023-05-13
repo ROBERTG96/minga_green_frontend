a5053b0562c1aaab41f559357cfa8466e6f99af5
ROBERTG96 <71914244+ROBERTG96@users.noreply.github.com>
2023-05-13 00:52:37 -0500

    Trabajando en NavBar - Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import NavBar from './components/shared/NavBar'
+import Main from './components/shared/Main'
+import Footer from './components/shared/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='w-screen h-screen bg-[#EBEBEB] container mx-auto px-4'>
+        <NavBar></NavBar>
+        <Main></Main>
+        <Footer></Footer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- src/components/shared/NavBar.jsx
+++ src/components/shared/NavBar.jsx
@@ -1,1 +1,53 @@
-<i class="fa fa-hourglass" aria-hidden="true"></i>
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <div >
+            <nav className='flex sm:justify-between p-3 flex-row-reverse mt-3 mr-4 '>
+                <div className='bg-black' >
+                    <img src="src/assets/img/NavBar/Minga.png" alt="" className='' />
+                </div>
+                <ul>
+                    <li >
+                    </li>
+                    <li>
+                        <a href="" className=''>Menu</a>
+                    </li>
+                </ul>
+            </nav>
+
+            <div className='grid grid-cols-3 gap-4 place-content-between h-48'>
+                <div>
+                    <img src="src/assets/img/NavBar/Rectangle605.png" className='w-[78.625rem] h-[16.563rem]' alt="" />
+                </div>
+                <div>
+                    <img src="src/assets/img/NavBar/8b8e139c764c05a681947b2d6855bd331.png" className='h-[19.188rem] w-[17.32rem]' alt="" />
+                </div>
+
+
+            </div>
+
+        </div>
+    )
+}
+
+/* 
+<div className=''>
+                    <img src="src/assets/img/NavBar/8b8e139c764c05a681947b2d6855bd331.png" className='h-[19.188rem] w-[17.32rem] ' alt="" srcset="" />
+                </div>
+
+                <img src="src/assets/img/NavBar/image3.png" className='h-72' alt="" srcset="" />
+*/
+
+/* prueba responsive
+ <section className='container mx-auto py-10 text-center bg-black text-white 
+            sm:bg-gray-300 sm:text-black 
+            md:bg-blue-600 md:text-white
+            lg:bg-pink-500
+            xl:bg-green-400
+            2xl:bg-yellow-500' >
+                NavBar
+            </section>
+             */
+
+export default NavBar
