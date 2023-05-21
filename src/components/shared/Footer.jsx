06c8f4a6a3a96e354097443b2210b7eea75d1276
ROBERTG96 <71914244+ROBERTG96@users.noreply.github.com>
2023-05-21 02:08:37 -0500

    Responsive Mobile - Desktop

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='bg-cyan-600'>Footer</div>
   )
 }
 
--- src/components/shared/NavBar.jsx
+++ src/components/shared/NavBar.jsx
@@ -3,14 +3,19 @@ import { RxHamburgerMenu } from "react-icons/rx";
 
 const NavBar = () => {
   return (
-    <nav className="flex">
-      <button className="">
-        <RxHamburgerMenu className="h-12 w-7 bg-gradient-to-tr text-[#F472B6]"></RxHamburgerMenu>
-      </button>
-      <div className="self-end">
-        <img src="/src/assets/img/NavBar/logo.svg" alt="" />
-      </div>
-    </nav>
+    <nav className="flex justify-between items-center">
+    <button className="mx-5 mb-4">
+      <RxHamburgerMenu className="h-10 w-10 bg-gradient-to-tr text-[#F472B6]"></RxHamburgerMenu>
+    </button>
+    <div className="my-5">
+      <img
+        src="/src/assets/img/NavBar/logo.svg"
+        className="w-[85%] my-5 md:block"
+        alt=""
+      />
+    </div>
+  </nav>
+
   );
 };
 
